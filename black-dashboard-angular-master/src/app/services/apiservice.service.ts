de1def219f369554f554d797a9ead7fdee43cc21
Matt Skeins <mbskeins@Matts-MacBook-Air.local>
2019-11-25 12:13:35 -0500

    im so sorry

--- black-dashboard-angular-master/src/app/services/apiservice.service.ts
+++ black-dashboard-angular-master/src/app/services/apiservice.service.ts
@@ -78,7 +78,6 @@ export class ApiService {
     }));
   }
 
-<<<<<<< HEAD
   addCadre(payload: any){
     var url = "http://127.0.0.1:8000/api/v1/cadres/";
     console.log("at api service");
@@ -89,8 +88,8 @@ export class ApiService {
     var url = "http://127.0.0.1:8000/api/v1/groups/";
     console.log("at api service");
     console.log(this.http.post(url, payload).subscribe(data => console.log(data)));
-=======
-
+  }
+  
   createSchool(payload: any){
     var url = "http://127.0.0.1:8000/api/v1/schools";
     console.log(this.http.post(url, payload).subscribe(data => console.log(data)));
@@ -106,7 +105,6 @@ export class ApiService {
     var url = "http://127.0.0.1:8000/api/v1/wings/";
     console.log(this.http.post(url, payload).subscribe(data => console.log(data)));
 
->>>>>>> 3c93635f8efa00de135ee280850896473c1086b7
   }
 
 }

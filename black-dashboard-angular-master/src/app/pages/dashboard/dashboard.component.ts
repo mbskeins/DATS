11ff29193a1c1a45eb88b45ad8e4b277dad7cfa6
Matt Skeins <mbskeins@Matts-MacBook-Air.local>
2019-12-03 23:22:13 -0500

    data binding

--- black-dashboard-angular-master/src/app/pages/dashboard/dashboard.component.ts
+++ black-dashboard-angular-master/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -6,6 +6,9 @@ import { ApiService } from 'src/app/services/apiservice.service';
 import Chart from 'chart.js';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/authservice.service';
+import { HttpHeaders } from '@angular/common/http';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 
 export interface UserData {
@@ -20,15 +23,6 @@ export interface EventData {
   progress: string;
 }
 
-/** Constants used to fill up our data base. */
-const COLORS: string[] = [
-  'green', 'red', 'orange', 'yellow',
-];
-const NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
-];
-
 const EVENTNAMES: string[] = [
   'LLAB', 'PT', "ALT LLAB", "FUN RUN", "OBSTACLE COURSE", "DRILL"
 ];
@@ -39,75 +33,93 @@ const EVENTNAMES: string[] = [
 @Component({
   selector: "app-dashboard",
   templateUrl: "dashboard.component.html",
-  styleUrls: ['dashboard.component.scss']
+  styleUrls: ['dashboard.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class DashboardComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'name', 'progress', 'color'];
   displayedColumns2: string[] = ['name', 'progress'];
 
-  dataSource: MatTableDataSource<UserData>;
+  dataSource;
   dataSource2: MatTableDataSource<EventData>;
+  $userList;
+  userlist;
+
+  $eventList;
+  eventList;
+
+  headers: HttpHeaders
+  columnsToDisplay = ['firstname', 'lastname', 'cellphone'];
+
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  @ViewChild(MatPaginator, { static: true }) paginator2: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort2: MatSort;
+  //@ViewChild(MatPaginator, { static: true }) paginator2: MatPaginator;
+  //@ViewChild(MatSort, { static: true }) sort2: MatSort;
 
-  constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) {
+  constructor(private apiService: ApiService, private authService: AuthService, private changeDetectorRefs: ChangeDetectorRef) { 
     // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
     const events = Array.from({ length: 5 }, (_, k) => createNewEvent(k + 1));
 
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
-
+    this.dataSource = new MatTableDataSource(this.userlist);
     this.dataSource2 = new MatTableDataSource(events);
   }
-
+ 
   ngOnInit() {
+
+    this.changeDetectorRefs.detectChanges();
+    this.headers = this.authService.getAuthHeaders();
+    this.$eventList = this.apiService.getEvents(this.headers);
+    this.$userList = this.apiService.getUsers(this.headers);
+
+    this.$eventList.subscribe(data => {
+      this.eventList = data;
+      console.log(this.eventList);
+    });
+
+    this.$userList.subscribe(data => {
+      this.userlist = data;
+      this.setTableData();
+      //console.log(this.userlist);
+    });
 
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
-
-    // for second table
-    this.dataSource2.paginator = this.paginator2;
-    this.dataSource2.sort = this.sort2;
-
   }
+
+  setTableData() {
+    this.dataSource = this.userlist;
+    console.log(this.dataSource);
+  }
+
 
   // for cadet attendance
   applyFilter(filterValue: string) {
+    console.log(filterValue);
     this.dataSource.filter = filterValue.trim().toLowerCase();
-
+    console.log(this.dataSource);
     if (this.dataSource.paginator) {
+      console.log("what");
       this.dataSource.paginator.firstPage();
     }
   }
 
   // for average event attendance
-  applyFilter2(filterValue: string) {
-    this.dataSource2.filter = filterValue.trim().toLowerCase();
+  // applyFilter2(filterValue: string) {
+  //   this.dataSource2.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource2.paginator) {
-      this.dataSource2.paginator.firstPage();
-    }
-  }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * (100 - 70) + 70).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
+  //   if (this.dataSource2.paginator) {
+  //     this.dataSource2.paginator.firstPage();
+  //   }
+  // }
 }
 
 function createNewEvent(id: number): EventData {
